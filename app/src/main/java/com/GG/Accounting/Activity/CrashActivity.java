c5b6a9318c6292bf705606754333cee29d39aaeb
GaodaGG <zil0718@outlook.com>
2023-11-26 00:44:40 +0800

    解决软键盘显示时切换再返回底栏位置不变的问题 提取Animation到单独类

--- app/src/main/java/com/GG/Accounting/Activity/CrashActivity.java
+++ app/src/main/java/com/GG/Accounting/Activity/CrashActivity.java
@@ -9,21 +9,18 @@ import android.os.Bundle;
 import android.util.TypedValue;
 import android.view.ViewGroup;
 import android.view.WindowInsets;
-import android.view.WindowInsetsAnimation;
 import android.widget.TextView;
 import android.widget.Toolbar;
 
-import androidx.annotation.NonNull;
 import androidx.cardview.widget.CardView;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
+import com.GG.Accounting.Animation.CrashAnimation;
 import com.GG.Accounting.R;
 import com.gyf.immersionbar.ImmersionBar;
 
 import net.center.blurview.ShapeBlurView;
 import net.center.blurview.enu.BlurMode;
-
-import java.util.List;
 
 import cn.qqtheme.framework.tool.CrashHelper;
 
@@ -49,22 +46,23 @@ public class CrashActivity extends Activity {
         Toolbar toolbar = findViewById(R.id.crash_toolbar);
         toolbar.setTitle(R.string.crashTitle);
 
-
+        //报错文本
         TextView textView = findViewById(R.id.crash_textview);
         textView.setText(String.format("%s %s %s", stackTrace, getString(R.string.devices), deviceInfo));
 
         //增加底栏高度
         ShapeBlurView blurView = findViewById(R.id.crash_blurview);
+
         int navigationBarHeight = getNavigationBarHeight(this);
         ViewGroup.LayoutParams blurViewLayoutParams = blurView.getLayoutParams();
         blurViewLayoutParams.height = ((int)TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 56, getResources().getDisplayMetrics())) + navigationBarHeight;
         blurView.setLayoutParams(blurViewLayoutParams);
+
         //底栏模糊
         blurView.refreshView(
                 ShapeBlurView.build(this).setBlurMode(BlurMode.MODE_RECTANGLE)
                         .setOverlayColor(Color.valueOf(255, 255, 255, 60).toArgb())
                         .setCornerRadius(25f)
-//                        .setCornerRadius(BlurCorner.TOP_LEFT, 50f)
         );
 
         CardView showIME = findViewById(R.id.crash_share);
@@ -72,22 +70,14 @@ public class CrashActivity extends Activity {
             showIME.setOnClickListener((view) -> view.getWindowInsetsController().show(WindowInsets.Type.ime()));
         }
 
-        animation();
-
+        CrashAnimation.animation(findViewById(R.id.crash_bottomview));
     }
 
-    private void animation() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-            ConstraintLayout bottomView = findViewById(R.id.crash_bottomview);
-            bottomView.setWindowInsetsAnimationCallback(new WindowInsetsAnimation.Callback(WindowInsetsAnimation.Callback.DISPATCH_MODE_STOP) {
-                @NonNull
-                @Override
-                public WindowInsets onProgress(@NonNull WindowInsets insets, @NonNull List<WindowInsetsAnimation> runningAnimations) {
-                    bottomView.setTranslationY(-insets.getInsets(WindowInsets.Type.ime()).bottom);
-                    return insets;
-                }
-            });
-        }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        //重设底栏位置
+        ConstraintLayout bottomView = findViewById(R.id.crash_bottomview);
+        bottomView.setTranslationY(0);
     }
-
 }
---
+++ app/src/main/java/com/GG/Accounting/Animation/CrashAnimation.java
@@ -0,0 +1,25 @@
+package com.GG.Accounting.Animation;
+
+import android.os.Build;
+import android.view.WindowInsets;
+import android.view.WindowInsetsAnimation;
+
+import androidx.annotation.NonNull;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import java.util.List;
+
+public class CrashAnimation {
+    public static void animation(ConstraintLayout bottomView) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            bottomView.setWindowInsetsAnimationCallback(new WindowInsetsAnimation.Callback(WindowInsetsAnimation.Callback.DISPATCH_MODE_STOP) {
+                @NonNull
+                @Override
+                public WindowInsets onProgress(@NonNull WindowInsets insets, @NonNull List<WindowInsetsAnimation> runningAnimations) {
+                    bottomView.setTranslationY(-insets.getInsets(WindowInsets.Type.ime()).bottom);
+                    return insets;
+                }
+            });
+        }
+    }
+}
