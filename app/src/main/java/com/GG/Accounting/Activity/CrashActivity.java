42143bf0546e7ab594b6b396af74f5d60b072835
GaodaGG <zil0718@outlook.com>
2023-11-26 03:16:44 +0800

    为报错页面两个按键添加应有的事件 尝试给模糊控件添加一层颜色

--- app/src/main/java/com/GG/Accounting/Activity/CrashActivity.java
+++ app/src/main/java/com/GG/Accounting/Activity/CrashActivity.java
@@ -3,6 +3,9 @@ package com.GG.Accounting.Activity;
 import static com.gyf.immersionbar.ImmersionBar.getNavigationBarHeight;
 
 import android.app.Activity;
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
@@ -21,6 +24,8 @@ import com.gyf.immersionbar.ImmersionBar;
 
 import net.center.blurview.ShapeBlurView;
 import net.center.blurview.enu.BlurMode;
+
+import java.util.Objects;
 
 import cn.qqtheme.framework.tool.CrashHelper;
 
@@ -44,33 +49,49 @@ public class CrashActivity extends Activity {
 
         //应用栏
         Toolbar toolbar = findViewById(R.id.crash_toolbar);
-        toolbar.setTitle(R.string.crashTitle);
+        toolbar.setTitle(R.string.crash_titleText);
 
         //报错文本
         TextView textView = findViewById(R.id.crash_textview);
-        textView.setText(String.format("%s %s %s", stackTrace, getString(R.string.devices), deviceInfo));
+        textView.setText(String.format("%s %s %s", stackTrace, getString(R.string.crash_devices), deviceInfo));
 
         //增加底栏高度
         ShapeBlurView blurView = findViewById(R.id.crash_blurview);
+        ConstraintLayout bottomView = findViewById(R.id.crash_bottomview);
 
         int navigationBarHeight = getNavigationBarHeight(this);
         ViewGroup.LayoutParams blurViewLayoutParams = blurView.getLayoutParams();
-        blurViewLayoutParams.height = ((int)TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 56, getResources().getDisplayMetrics())) + navigationBarHeight;
+        blurViewLayoutParams.height = ((int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 56, getResources().getDisplayMetrics())) + navigationBarHeight;
         blurView.setLayoutParams(blurViewLayoutParams);
 
         //底栏模糊
         blurView.refreshView(
                 ShapeBlurView.build(this).setBlurMode(BlurMode.MODE_RECTANGLE)
-                        .setOverlayColor(Color.valueOf(255, 255, 255, 60).toArgb())
-                        .setCornerRadius(25f)
+                        .setOverlayColor(Color.parseColor("#FFFFFF"))
         );
 
-        CardView showIME = findViewById(R.id.crash_share);
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-            showIME.setOnClickListener((view) -> view.getWindowInsetsController().show(WindowInsets.Type.ime()));
-        }
 
-        CrashAnimation.animation(findViewById(R.id.crash_bottomview));
+        //保存报错
+        CardView saveErrorButton = findViewById(R.id.crash_save);
+        saveErrorButton.setOnClickListener((view) -> {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+                Objects.requireNonNull(view.getWindowInsetsController()).show(WindowInsets.Type.ime());
+            }
+            ClipboardManager clipboardManager = (ClipboardManager) getSystemService(CLIPBOARD_SERVICE);
+            clipboardManager.setPrimaryClip(ClipData.newPlainText(null, textView.getText()));
+        });
+
+        //重启软件
+        CardView restartButton = findViewById(R.id.crash_restartApp);
+        restartButton.setOnClickListener((view) -> {
+            final Intent intent = getPackageManager().getLaunchIntentForPackage(getPackageName());
+            assert intent != null;
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            startActivity(intent);
+        });
+
+        //底栏动画
+        CrashAnimation.animation(bottomView);
     }
 
     @Override
